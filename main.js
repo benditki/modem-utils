959dc7e96519ab0f24d45f034cf3b475b4cd0828
Ilia Benditkis <iliab@mobileye.com>
2018-05-22 09:17:22 +0300

    added gui with table of ports (as a try)

---
+++ main.js
@@ -0,0 +1,31 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 1200, height: 600})
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  //mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+app.on('window-all-closed', function () {
+  app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
