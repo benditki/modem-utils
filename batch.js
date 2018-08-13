cb43048b1d6d74d0c8600c1a855901ebf7079afe
Ilia Benditkis <iliab@mobileye.com>
2018-08-13 15:12:39 +0300

    first working version for batch (still separated from manual)

--- batch.js
+++ batch.js
@@ -8,7 +8,7 @@ gsms = {}
 
 const scan_ports = util.promisify(serialport.list)
 
-localStorage.debug = "=D="
+localStorage.debug = "" //"=D="
 
 function $(id) { return document.getElementById(id) }
 
@@ -17,34 +17,26 @@ var ractive = new Ractive({
     template: '#page-tpl',
     data: {
         show_grid: false,
-    },
-
-    stored: [],
-
-    async oninit() {
-        for (var keypath of this.stored) {
-            try {
-                if (localStorage[keypath]) {
-                    this.set(keypath, JSON.parse(localStorage[keypath]))
-                }
-            } catch (e) {
-                if (e instanceof SyntaxError) {
-                    this.set(keypath, localStorage[keypath])
-                } else {
-                    throw e
-                }
-            }
-        }
-
-    },
+    }
 });
 
-window.onbeforeunload = function () {
-    for (keypath of ractive.stored) { 
-        localStorage[keypath] = ractive.get(keypath) || ''
+var baudrate_map = {}
+
+try {
+    baudrate_map = JSON.parse(localStorage.baudrate_map)
+}
+catch (e) {
+    if (!e instanceof SyntaxError) {
+        throw e
     }
 }
+
+window.onbeforeunload = function () {
+    localStorage.baudrate_map = JSON.stringify(baudrate_map)
+}
 window.ractive = ractive
+
+
 
 
 function log(msg) {
@@ -62,8 +54,27 @@ function log(msg) {
     elem.scrollTop = elem.scrollHeight
 }
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+var desired_baudrate = 921600
+
+function make_first(first, list) {
+    var res = [first]
+    for (var item of list) {
+        if (item != first) {
+            res.push(item)
+        }
+    }
+    return res
+}
+
 async function connect(port_name) {
     var baudrates = [115200, 921600]
+    if (baudrate_map[port_name]) {
+        baudrates = make_first(baudrate_map[port_name], baudrates)
+    }
     var key = "gsm." + port_name + "."
     var gsm = gsms[port_name]
     try {
@@ -72,44 +83,68 @@ async function connect(port_name) {
         
         await ractive.set(key + "active_baudrate", baudrate)
         await ractive.set(key + "state", 1)
+
+        if (baudrate != desired_baudrate) {
+            await gsm.command(`AT+IPR=${desired_baudrate}`)
+            baudrate = await gsm.connect(port_name, make_first(desired_baudrate, baudrates))
+            await gsm.command("AT&W;+CFUN=15")
+            await sleep(4000)
+            await gsm.command("AT", { timeout: 200, repeat: 20 });
+        }
+        
+        baudrate_map[port_name] = baudrate
+        
+        await ractive.set(key + "active_baudrate", baudrate)
+        await ractive.set(key + "state", 2)
         
         var response;
         response = await gsm.command("ATI")
         await ractive.set(key + "model", response.body)
-        response = await gsm.command("AT+GSN;+CCID")
+        response = await gsm.command("AT+GSN")
         await ractive.set(key + "sn", response.body)
-        await ractive.set(key + "sim_id", response.sim_id)
-        await ractive.set(key + "state", 2)
-
-        response = await gsm.command("AT+UCGOPS?;+CGATT?", {
-            response_valid: response => response && response.operator && response.network && response.gprs_attached == "1",
-            timeout: 2000
-        })
-        await ractive.set("operator", response.operator)
-        await ractive.set("network", response.network)
-        await ractive.set("gprs_attached", response.gprs_attached == "1")
         await ractive.set(key + "state", 3)
-
+        
         response = await gsm.command("AT+UPSND=0,8")
         if (response.status == "0") {
             await gsm.command("AT+UPSDA=0,2")
         }
 
         response = await gsm.command("AT+UPSD=0,1;+UPSD=0,7")
-        await ractive.set("stored_apn", response.apn)
-        await ractive.set("current_apn", response.apn)
-        await ractive.set("stored_ip", response.stored_ip)
-        await ractive.set(key + "state", 4)
-
-        response = await gsm.command("AT+UPSND=0,8")
-        if (response.status == "0") {
-            await gsm.command("AT+UPSDA=0,3")
+        if (response.apn != "mobileye8") {
+            response = await gsm.command("ATE0;V0;&K0;&W;+UPSD=0,1,\"mobileye8\";+UPSD=0,7,\"0.0.0.0\";+UPSDA=0,1;+CFUN=15")
+            await sleep(4000)
+            await gsm.command("AT", { timeout: 200, repeat: 20 });
+            await gsm.command("AT+UPSDA=0,2")
+            response = await gsm.command("AT+UPSD=0,1;+UPSD=0,7")
         }
+            
+        await ractive.set(key + "apn", response.apn)
+        await ractive.set(key + "static_ip", response.stored_ip)
 
-        response = await gsm.command("AT+UPSND=0,0")
-        await ractive.set("current_ip", response.ip)
-        await ractive.set(key + "state", 5)
-        
+        response = await gsm.command("AT+CCID")
+        await ractive.set(key + "sim_id", response.sim_id)
+        await ractive.set(key + "state", 4)
+        if (response.sim_id) {
+            await ractive.set(key + "state", 5)
+            response = await gsm.command("AT+UCGOPS?;+CGATT?", {
+                response_valid: response => response && response.operator && response.network && response.gprs_attached == "1",
+                timeout: 2000
+            })
+            await ractive.set("operator", response.operator)
+            await ractive.set("network", response.network)
+            await ractive.set("gprs_attached", response.gprs_attached == "1")
+            await ractive.set(key + "state", 6)
+
+            response = await gsm.command("AT+UPSND=0,8")
+            if (response.status == "0") {
+                await gsm.command("AT+UPSDA=0,3")
+            }
+
+            response = await gsm.command("AT+UPSND=0,0")
+            await ractive.set("current_ip", response.ip)
+            await ractive.set(key + "state", 7)
+        }
+            
         setTimeout(check, 200, port_name)
     }
     catch (e) {
@@ -135,9 +170,18 @@ async function check(port_name) {
 
 async function refresh_ports() {
     var exclude_ports = ["COM1"]
-    var port_names = (await scan_ports()).map(port => port.comName)
+    var ports = await scan_ports()
+    //console.log(ports)
+    var serial_seen = {}
+    var port_names = [];
+    for (port of ports) {
+        if (exclude_ports.includes(port.comName)) continue;
+        if (serial_seen[port.serialNumber]) continue;
+        serial_seen[port.serialNumber] = true
+        port_names.push(port.comName)
+    }
     
-    for (var port_name of port_names.filter(name => !exclude_ports.includes(name))) {
+    for (var port_name of port_names) {
         if (!gsms[port_name]) {
             var gsm = new GSM(log)
             gsms[port_name] = gsm
