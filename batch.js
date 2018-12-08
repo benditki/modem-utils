085d65660219d46e9ae3b79e56e2329cc5154105
Ilia Benditkis <benditki@gmail.com>
2018-12-08 17:04:40 +0200

    bench: manual and automatic restart (after 5 sec - hardcoded)

--- batch.js
+++ batch.js
@@ -63,7 +63,33 @@ var ractive = new Ractive({
         <i class="la-line-scale-pulse-out-rapid la-dark la-sm"><div></div><div></div><div></div><div></div><div></div></i>
         <span class="progress">${label}</span>
         <i class="la-line-scale-pulse-out-rapid la-dark la-sm"><div></div><div></div><div></div><div></div><div></div></i>`
+    },
+    on: {
+        async restart(context, port_name) {
+            console.log("restarting", port_name)
+            var gsm = gsms[port_name]
+            if (gsm) {
+                await gsm.command("AT+CFUN=15")
+            }
+        }
+    },
+    format_countdown(ts) {
+        var left = Math.round((ts - Date.now()) / 1000)
+        var fractions = []
+        for (factor of [60, 60, 24]) {
+            if (fractions.length >= 2 && !left) {
+                break
+            }
+            fractions.push(left % factor)
+            left = Math.floor(left / factor)
+        }
+        if (fractions.length < 2 || left) {
+            fractions.push(left)
+        }
+        fractions = fractions.map((f, i, a) => ( i < 2 && i < a.length - 1 ? f.toString().padStart(2, '0') : f ))
+        return fractions.reverse().join(':')
     }
+
 });
 
 var baudrate_map = {}
@@ -162,7 +188,6 @@ async function connect(port_name) {
         }
 
         response = await gsm.command("AT+UPSD=0,1;+UPSD=0,7;&V")
-        console.log(response)
         if (response.apn != "mobileye8" || response.stored_ip != "0.0.0.0" ||
                 response.flow_control != "0" || response.echo != "0" || response.verbose != "0") {
             if (gprs_activated) {
@@ -241,6 +266,7 @@ async function connect(port_name) {
             
         }
         
+        await ractive.set(key + "restart_ts", Date.now() + 5 * 1000)
         await ractive.set(key + "done", true)
             
         setTimeout(check, 200, port_name)
@@ -259,7 +285,21 @@ async function disconnect(port_name) {
 async function check(port_name) {
     if (!gsms[port_name]) return;
     try {
-        await gsms[port_name].command("AT", {timeout: 200, repeat: 1})
+        var restarted = false;
+        var restart_ts_key = 'gsm.' + port_name + '.restart_ts'
+        if (ractive.get(restart_ts_key)) {
+            var left = ractive.get(restart_ts_key) - Date.now()
+            if (left > 0) {
+                await ractive.update(restart_ts_key, { force: true })
+            } else {
+                await gsms[port_name].command("AT+CFUN=15")
+                ractive.set(restart_ts_key, null)
+                restarted = true
+            }
+        }
+        if (!restarted) {
+            await gsms[port_name].command("AT", {timeout: 200, repeat: 1})
+        }
         setTimeout(check, 400, port_name)
     } catch (e) {
         log("ERROR: " + e.message)
@@ -268,7 +308,7 @@ async function check(port_name) {
 }
 
 async function refresh_ports() {
-    var exclude_ports = ["COM1", "COM3", "COM26", "COM36"]
+    var exclude_ports = ["COM1"]
     var ports = await scan_ports()
 
     var port_names = [];
